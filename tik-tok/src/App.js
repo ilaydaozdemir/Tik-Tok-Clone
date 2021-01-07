bb838a1b35549df1dfc22d8612d71696d58e9a01
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-01-08 00:33:03 +0300

    start

---
+++ tik-tok/src/App.js
@@ -0,0 +1,12 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Tik Tok Clone</h1>
+      <strong>ILAYDA ÖZDEMIR</strong>
+    </div>
+  );
+}
+
+export default App;
